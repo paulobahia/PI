fa5522bd07550deb60fe10a8ad06d519b89ede15
Paolo <pauloipatinga22@gmail.com>
2022-10-29 15:09:41 -0300

    FIX

--- src/pages/LandingPage/components/Contact.jsx
+++ src/pages/LandingPage/components/Contact.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import styles from "../../../style";
 
 const Contact = () => {
-    return <div className={`${styles.paddingY}  flex-col relative `}>Contact</div>;
+    return <div className={`${styles.paddingY}  flex-col relative `}>
+        Tela de Contato
+    </div>;
 }
 
 export default Contact
