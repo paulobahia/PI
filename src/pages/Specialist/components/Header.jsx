0d2accbc95b7534b1d5c6d812c7293b0db056c7e
Paulo <you@example.com>
2022-11-12 15:52:58 -0300

    FIX

--- src/pages/Specialist/components/Header.jsx
+++ src/pages/Specialist/components/Header.jsx
@@ -3,9 +3,7 @@ import styles from "../../../style";
 import { people } from "../../../../assets";
 
 const Header = () => (
-  <section className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-    Master
-  </section>
+  <section className={`flex md:flex-row flex-col ${styles.paddingY}`}></section>
 );
 
 export default Header;
